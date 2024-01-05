0b96beddf4c449d6eee4238fca2c0a161f4a9ba8
Brahiam E. Quintero T <brahiamquintero@hotmail.com>
2024-01-05 11:50:51 -0500

    Initial Project Setup and Configuration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ROUTES } from './config/routes.constants';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: ROUTES.LOGIN,
+    loadChildren: () =>
+      import('./pages/login/login.module').then((m) => m.LoginModule),
+  },
+  {
+    path: ROUTES.EDIT,
+    loadChildren: () =>
+      import('./pages/edit/edit.module').then((m) => m.EditModule),
+  },
+  {
+    path: '**',
+    redirectTo: ROUTES.LOGIN,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
